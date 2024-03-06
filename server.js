113651216647220d7d886f1286b1bf1e520ce3f5
ismaildevgame <faizanahmad10@gmail.com>
2024-03-06 17:08:02 +0530

    dd

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ mongoose.connect(process.env.MONGODB);
 var fs = require("fs");
 var product = require("./model/product.js");
 var user = require("./model/user.js");
+var path=require('path');
+
 
 
 const corsOpts = {
@@ -30,30 +32,27 @@ const corsOpts = {
 var dir = "./uploads";
 var upload = multer({
   storage: multer.diskStorage({
+
     destination: function (req, file, callback) {
       if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
       }
-      callback(null, "./uploads");
+      callback(null, './uploads');
     },
-    filename: function (req, file, callback) {
-      callback(
-        null,
-        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-      );
-    },
+    filename: function (req, file, callback) { callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); }
+
   }),
 
   fileFilter: function (req, file, callback) {
-    var ext = path.extname(file.originalname);
-    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
-      return callback(/*res.end('Only images are allowed')*/ null, false);
+    var ext = path.extname(file.originalname)
+    if (ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+      return callback(/*res.end('Only images are allowed')*/ null, false)
     }
-    callback(null, true);
-  },
+    callback(null, true)
+  }
 });
 app.use(cors());
-app.use("/uploads", express.static("uploads"));
+app.use(express.static(path.join(__dirname,'uploads')));
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(
   bodyParser.urlencoded({
